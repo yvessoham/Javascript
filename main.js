59e86ca96b73904d10288ff6efe62b63acdd88ce
riadh-trabelsi <143900385+riadh-trabelsi@users.noreply.github.com>
2023-10-24 17:10:47 +0200

    new

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-import './style.css'
 
+(() => {
+    let score = 0;
+    let multi1 = 1;
+  
+    const cookiescore = document.getElementById("score");
+    const clickbutton = document.getElementById("buton");
+    const multipli1 = document.getElementById("multi1");
+  
+    clickbutton.addEventListener("click", function () {
+      score += multi1;
+      cookiescore.textContent = score;
+    });
+  })();
 
